5c5a3f6c40819e50488b8ff2120d738db4e5ba6f
KakwiriStephen <ngottakakwiri@gmail.com>
2022-10-24 12:25:12 +0300

    Searching

---
+++ Searching/linearSearch.js
@@ -0,0 +1,32 @@
+//linear Search
+//ONE element at a time
+
+function linearSearch(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) return i;
+    }
+    return -1;
+}
+linearSearch([1, 2, 3, 4, 5, 6, 7], 6);
+
+//6 is at position 5 Hence 5 is returned
+
+//Binary linearSearch
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+    while (arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if (arr[middle] === elem) {
+        return middle;
+    }
+    return -1;
+}
+binarySearch([1, 2, 5, 6, 7, 9, 11, 34], 11);
