3e626d9264fca4bde524a5c1567c4ed76a9ca83d
KakwiriStephen <ngottakakwiri@gmail.com>
2022-10-24 19:50:09 +0300

    Naive Search

--- Searching/linearSearch.js
+++ Searching/linearSearch.js
@@ -30,3 +30,19 @@ function binarySearch(arr, elem) {
     return -1;
 }
 binarySearch([1, 2, 5, 6, 7, 9, 11, 34], 11);
+
+// Naive Search
+
+function naiveSearch(long, short) {
+    var count = 0;
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (short[j] !== long[i + j]) break;
+            if (j === short.length - 1) count++;
+        }
+    }
+    return count;
+}
+naiveSearch("lorie loled", "lo");
+
+// returns a count of 2
