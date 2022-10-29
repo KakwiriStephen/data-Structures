e747ffd2d362b0945ea1690dc98de614e2854608
KakwiriStephen <ngottakakwiri@gmail.com>
2022-10-29 22:31:34 +0300

    Bubble sort

--- BubbleSort/bubbleSort.js
+++ BubbleSort/bubbleSort.js
@@ -1,11 +1,13 @@
 function bubbleSort(arr) {
     let noSwaps;
+    //If the array is already sorted
 
     for (let i = arr.length; i > 0; i--) {
         noSwaps = true;
         for (let j = 0; j < i - 1; j++) {
             console.log(arr);
             if (arr[j] > arr[j + 1]) {
+                //swap
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
