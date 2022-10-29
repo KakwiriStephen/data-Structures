00663689e112b402c75004dc7aefd343719d4145
KakwiriStephen <ngottakakwiri@gmail.com>
2022-10-29 22:26:44 +0300

    Bubble Sort

---
+++ BubbleSort/bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    let noSwaps;
+
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            console.log(arr);
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return arr;
+}
+bubbleSort([37, 45, 29, 8, 24, 6]);
