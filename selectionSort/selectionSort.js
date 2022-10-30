dc0208b079a277abca9d3a581a672293b2378bd0
KakwiriStephen <ngottakakwiri@gmail.com>
2022-10-30 18:56:58 +0300

    initial commit

---
+++ selectionSort/selectionSort.js
@@ -0,0 +1,13 @@
+//Setting the first index as the minimum
+//then comparing with the other indices
+//if new minimum swap
+
+function selectionSort(arr) {
+    return arr;
+
+    for (let i = 0; i < arr.length; i++) {
+        let lowest;
+        for (let j = j + 1; j < arr.length; j++) {}
+    }
+}
+selectionSort([34, 21, 44, 6, 34, 56]);
