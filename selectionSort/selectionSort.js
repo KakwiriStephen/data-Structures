88ad0374a60fcb095da9b71e1b6126dab80d3f05
KakwiriStephen <ngottakakwiri@gmail.com>
2022-10-30 23:47:32 +0300

    Selection Sort

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -3,11 +3,19 @@
 //if new minimum swap
 
 function selectionSort(arr) {
-    return arr;
-
     for (let i = 0; i < arr.length; i++) {
-        let lowest;
-        for (let j = j + 1; j < arr.length; j++) {}
+        let lowest = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+        }
+        if (i !== lowest) {
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
     }
+    return arr;
 }
 selectionSort([34, 21, 44, 6, 34, 56]);
