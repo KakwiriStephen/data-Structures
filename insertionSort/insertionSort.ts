a32d04b68fa4f77acd51f0ac7e1937977c95b4cc
KakwiriStephen <ngottakakwiri@gmail.com>
2022-10-31 20:29:48 +0300

    initial commit

---
+++ insertionSort/insertionSort.ts
@@ -0,0 +1,14 @@
+//sortimg the left hand side
+
+function insertionSort(arr){
+    for(var i=1; i<arr.length; i++){
+        var currentVal=arr[i];
+        for(var j =i-1; j>=0 && arr[j] > currentVal; j--){
+         arr[j+1]=arr[j]   
+        }
+        arr[j+1] = currentVal;
+    }
+    
+    return arr;
+}
+insertionSort([9,7,5,2,1])
