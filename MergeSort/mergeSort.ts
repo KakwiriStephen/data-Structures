099d86809183dc0b83600e29181857e0cedbd88e
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-03 14:03:30 +0300

    merge sort

--- MergeSort/mergeSort.ts
+++ MergeSort/mergeSort.ts
@@ -29,3 +29,48 @@ function mergeTwo(arr1,arr2){
     return results;
 }
 mergeTwo([1,4,10,14,50],[2,14,16,99])
+
+
+//full implementtation
+//1
+//Merging two sorted arrays
+
+function merge(arr1,arr2){
+
+    let results=[];
+    let i=0;
+    let j=0;
+
+    while(i<arr1.length && j<arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i])
+            i++
+        }
+        else{
+            results.push(arr2[j])
+            j++
+        }
+    }
+    while(j<arr2.length){
+        results.push(arr2[j])
+        j++
+    }
+    while(i<arr1.length){
+        results.push(arr1[i])
+        i++
+    }
+
+    return results;
+}
+
+function mergeSort(arr){
+    if(arr.length <=1)return arr;
+    let mid= Math.floor(arr.length/2);
+    let left=mergeSort(arr.slice(0,mid));
+    let right=mergeSort(arr.slice(mid))
+
+    return merge(left,right)
+    
+}
+
+mergeSort([10,24,76,73])
