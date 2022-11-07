ae083ae0a2b4c338184337a097e8bd4826a59e15
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-07 23:24:18 +0300

    helper function

--- Radixsort/radixSort.js
+++ Radixsort/radixSort.js
@@ -10,3 +10,13 @@ function getDigit(num) {
     if (num === 0) return 1;
     return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
+
+//.3)returning the number of digits in the largest numbers in the list
+
+function mostDigits(nums) {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+}
