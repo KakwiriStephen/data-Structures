ed4285477d8a0e0de987d8abe8994f9255b1730c
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-07 23:10:43 +0300

    Helper functions

--- Radixsort/radixSort.js
+++ Radixsort/radixSort.js
@@ -1,6 +1,12 @@
 //helper functions
-//helper function to return the digit in num at the given place value
+//.1)helper function to return the digit in num at the given place value
 
 function getDigit(num, i) {
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+//.2)digit count to return the number of digits in a number
+function getDigit(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
