5f8e27ac75e431ec13d07a1bf0169d1671760e1e
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-09 11:15:10 +0300

    popping nodes

--- SingleLinkedLists/singlelinkedLists.js
+++ SingleLinkedLists/singlelinkedLists.js
@@ -13,27 +13,44 @@ class singlyLinkedList {
         this.length = 0;
     }
     push(val) {
-        var newNode = new Node(val);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
+            var newNode = new Node(val);
+            if (!this.head) {
+                this.head = newNode;
+                this.tail = this.head;
+            } else {
+                this.tail.next = newNode;
+                this.tail = newNode;
+            }
+            this.length++;
+            return this;
         }
-        this.length++;
-        return this;
+        //method to traverse a list
+        // traverse() {
+        //     var current = this.head;
+        //     while (current) {
+        //         current = current.next;
+        //     }
+
+    // }
+
+    //popping a node  in a list
+    pop() {
+        if (!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length == 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
     }
 }
 var list = new singlyLinkedList();
 list.push("Hello");
-
-
-//method to traverse a list
-traverse() {
-    var current = this.head;
-    while (current) {
-        current = current.next;
-    }
-
-}
