2175528e17ba34c9f10669559073a5e7df9c4e98
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-10 21:03:33 +0300

    Remove

--- SingleLinkedLists/singlelinkedLists.js
+++ SingleLinkedLists/singlelinkedLists.js
@@ -114,6 +114,18 @@ class singlyLinkedList {
         this.length++;
         return true;
     }
+
+    //Remove
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+        var previousNode = this.get(index - 1);
+        var removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
 }
 var list = new singlyLinkedList();
 list.push("Hello");
