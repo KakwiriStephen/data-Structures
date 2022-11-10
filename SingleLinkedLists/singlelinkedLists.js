729c1eb06cbce44975cb66f82ee11a196880a744
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-10 13:50:58 +0300

    Set-change value

--- SingleLinkedLists/singlelinkedLists.js
+++ SingleLinkedLists/singlelinkedLists.js
@@ -80,14 +80,24 @@ class singlyLinkedList {
 
     //get by index
     get(index) {
-        if (index < 0 || index > this.length) return null;
-        var counter = 0;
-        var current = this.head;
-        while (counter !== index) {
-            current = current.next;
-            counter++;
+            if (index < 0 || index > this.length) return null;
+            var counter = 0;
+            var current = this.head;
+            while (counter !== index) {
+                current = current.next;
+                counter++;
+            }
+            return current;
         }
-        return current;
+        //set
+        //changing the value of a node
+    set(index, val) {
+        var foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
     }
 }
 var list = new singlyLinkedList();
