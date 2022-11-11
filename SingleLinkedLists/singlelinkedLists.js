602923213d332733b5c5935306b68ea872e5148d
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-11 16:02:28 +0300

    Reversing a list

--- SingleLinkedLists/singlelinkedLists.js
+++ SingleLinkedLists/singlelinkedLists.js
@@ -127,6 +127,20 @@ class singlyLinkedList {
             return removed;
         }
         //reversing a singly linked list
+    reverse() {
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        var next;
+        var prev = null;
+        for (var i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
 }
 var list = new singlyLinkedList();
 list.push("Hello");
