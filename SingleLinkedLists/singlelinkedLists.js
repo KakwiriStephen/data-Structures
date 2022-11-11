bf3cbc84d9510e4ff865894efb410b0c7a118276
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-11 22:18:10 +0300

    Singly linked list

--- SingleLinkedLists/singlelinkedLists.js
+++ SingleLinkedLists/singlelinkedLists.js
@@ -117,16 +117,17 @@ class singlyLinkedList {
 
     //Remove
     remove(index) {
-            if (index < 0 || index >= this.length) return undefined;
-            if (index === 0) return this.shift();
-            if (index === this.length - 1) return this.pop();
-            var previousNode = this.get(index - 1);
-            var removed = previousNode.next;
-            previousNode.next = removed.next;
-            this.length--;
-            return removed;
-        }
-        //reversing a singly linked list
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+        var previousNode = this.get(index - 1);
+        var removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+
+    //reversing a singly linked list
     reverse() {
         var node = this.head;
         this.head = this.tail;
