03589b3959e01b9aa65851ae0c83aab0acacc6f0
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-15 00:11:32 +0300

    Pop Method

--- DoubleLinkedList/doubleLinkedList.ts
+++ DoubleLinkedList/doubleLinkedList.ts
@@ -39,4 +39,20 @@ class DoublyLinkedlist{
         this.length++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined;
+        var poppedNode=this.tail;
+        if(this.length===1){
+            this.head=null;
+            this.tail=null;
+        }else{
+            this.tail=poppedNode.prev;
+            this.tail.next=null;
+            poppedNode.prev=null
+        }
+        this.length--;
+        return poppedNode
+    }
+
 }
