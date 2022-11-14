5df09c3c1989a2d4844fadea8774035569a2be4c
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-14 23:01:20 +0300

    Node class

---
+++ DoubleLinkedList/doubleLinkedList.ts
@@ -0,0 +1,21 @@
+/*A double linked list has two classes
+Node Class which contains
+-val
+-next
+-prev
+
+DoublyLinkedlist
+-head
+-tail
+length
+*/
+
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null;
+        this.prev=null;
+
+    }
+
+}
