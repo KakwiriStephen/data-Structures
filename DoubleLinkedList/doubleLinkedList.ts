99e9f3ed9b621ca3fc6a307f362115863c89ddf5
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-15 21:06:58 +0300

    Shift method

--- DoubleLinkedList/doubleLinkedList.ts
+++ DoubleLinkedList/doubleLinkedList.ts
@@ -55,5 +55,20 @@ class DoublyLinkedlist{
         this.length--;
         return poppedNode;
     } 
+    //shift method
+    shift(){
+        if (this.length===0) return undefined;
+        var oldHead=this.head;
+        if(this.length===1){
+            this.head=null;
+            this.tail=null;
+        }
+        this.head=oldHead.next;
+        this.head.prev=null;
+        oldHead.next=null;
+        this.length--;
+        return oldHead
+
+    }
 
 }
