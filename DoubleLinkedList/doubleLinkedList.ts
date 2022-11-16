ee8393f9b292916367e2c0333b1c9d5ed1fe20da
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-16 23:04:54 +0300

    Unshift

--- DoubleLinkedList/doubleLinkedList.ts
+++ DoubleLinkedList/doubleLinkedList.ts
@@ -72,5 +72,18 @@ class DoublyLinkedlist{
     }
 
     //Umshift method
+    unshift(val){
+        var newNode=new Node(val);
+        if (this.length===0){
+            this.head=newNode;
+            this.tail=newNode;
+        }else{
+            this.head.prev=newNode;
+            newNode.next = this.head;
+            this.head=newNode;
+        }
+        this.length++;
+        return this
+    }
 
 }
