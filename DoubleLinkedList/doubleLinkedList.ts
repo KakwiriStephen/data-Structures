7126dffa1e1a34088463c937ff3dfae7546bf6b0
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-17 23:31:56 +0300

    Remove

--- DoubleLinkedList/doubleLinkedList.ts
+++ DoubleLinkedList/doubleLinkedList.ts
@@ -138,7 +138,20 @@ class DoublyLinkedlist{
     }
     //Remove
     remove(index){
-        
+        if(index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length) return this.pop();
+
+        var removedNode = this.get(index)
+
+        removedNode.prev.next= removedNode.next;
+        removedNode.next.prev=removedNode.prev;
+        removedNode.next=null;
+        removedNode.prev=null;
+        this.length--;
+        return removedNode;
+
+
     }
 
 }
