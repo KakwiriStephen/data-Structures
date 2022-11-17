8b2898ebc44e72022525a4e997024d6732cb5e5d
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-17 21:27:51 +0300

    Set Method

--- DoubleLinkedList/doubleLinkedList.ts
+++ DoubleLinkedList/doubleLinkedList.ts
@@ -106,5 +106,16 @@ class DoublyLinkedlist{
             return current;
         }
     }
+    //set
+    //setting the value of a node
+
+    set(index, val){
+        var foundNode = this.get(index);
+        if (foundNode != null){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 
 }
