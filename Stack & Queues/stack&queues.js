48a3f01ab4ee8da309233984029680cebbdca84e
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-18 23:55:38 +0300

    Push

---
+++ Stack & Queues/stack&queues.js
@@ -0,0 +1,29 @@
+//Stack and Queues
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        var newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+}
