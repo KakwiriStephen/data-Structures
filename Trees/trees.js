a7dc15c0a1226764c749524125532d244491453f
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-20 18:28:58 +0300

    Insertion

--- Trees/trees.js
+++ Trees/trees.js
@@ -21,3 +21,18 @@ tree.root.left = new Node(7);
 tree.root.left.right = new Node(9);
 
 //////////////////////////////////////////////////////////
+
+// insertion
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
