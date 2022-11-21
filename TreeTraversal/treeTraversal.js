902001ec09d4461189919d40957014d436a75220
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-21 23:11:56 +0300

    BFS

--- TreeTraversal/treeTraversal.js
+++ TreeTraversal/treeTraversal.js
@@ -45,6 +45,9 @@ class BinarySearchTree {
         queue.push(node);
         while (queue.length) {
             node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
         }
     }
 }
