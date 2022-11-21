8967762ddcbb8797a2a8d330bd952e48fa556894
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-21 22:59:45 +0300

    Breadth First Search

---
+++ TreeTraversal/treeTraversal.js
@@ -0,0 +1,43 @@
+//Tree traversal
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            while (true) {
+                // handling Duplicates
+                if (value === current.value) return undefined;
+                //left
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                    //right
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    //Tree traversal
+    //BreadthWise
+    BFS() {}
+}
+
+var tree = new BinarySearchTree();
