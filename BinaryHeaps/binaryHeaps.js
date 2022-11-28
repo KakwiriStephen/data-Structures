0833bdcdd87d1f54daeed7ca8e764c68d8ac6bae
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-28 07:26:13 +0300

    Insert

--- BinaryHeaps/binaryHeaps.js
+++ BinaryHeaps/binaryHeaps.js
@@ -4,10 +4,24 @@
 
 class MaxBinaryHeap {
     constructor() {
-        this.value = [];
+        this.value = [41, 39, 33, 18, 27, 12];
     }
     insert(element) {
         this.values.push(element);
         this.bubbleUp();
     }
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while (true) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.value[parentIdx];
+            if (element > parent) {
+                this.values[parentIdx] = element;
+                this.values[idx] = parent;
+            }
+        }
+    }
 }
+let heap = new MaxBinaryHeap();
+heap.insert(55);
