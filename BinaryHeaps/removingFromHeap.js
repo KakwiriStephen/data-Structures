b84e2a4010624cbf2e5b96c20d1de26611051be7
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-29 18:54:45 +0300

    Method edge Case

--- BinaryHeaps/removingFromHeap.js
+++ BinaryHeaps/removingFromHeap.js
@@ -1,1 +1,37 @@
 //Extracting the maximum in a max-heap or the minimum in a min-heap then resort
+class MaxBinaryHeap {
+    constructor() {
+        this.value = [41, 39, 33, 18, 27, 12];
+    }
+    insert(element) {
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.value[parentIdx];
+            if (element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+        }
+    }
+
+    //extract Max
+    extractMax(){
+        const max = this.value[0];
+        const end=this.values.pop();
+        this.values[0] = end;
+        return max;
+    }
+
+}
+
+let heap = new MaxBinaryHeap();
+heap.insert(55);
+heap.insert(3);
+heap.insert(27);
+heap.insert(15);
+heap.insert(12);
