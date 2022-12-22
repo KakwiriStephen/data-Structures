cd70414d084185943f72b4e6c2236e2af63e08cc
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-22 22:24:02 +0300

    DSA

--- Searching/linearSearch.js
+++ Searching/linearSearch.js
@@ -2,46 +2,46 @@
 //ONE element at a time
 
 function linearSearch(arr, val) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === val) return i;
-    }
-    return -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+  return -1;
 }
 linearSearch([1, 2, 3, 4, 5, 6, 7], 6);
 
 //6 is at position 5 Hence 5 is returned
+//
 
 //Binary linearSearch
 function binarySearch(arr, elem) {
-    let start = 0;
-    let end = arr.length - 1;
-    let middle = Math.floor((start + end) / 2);
-    while (arr[middle] !== elem && start <= end) {
-        if (elem < arr[middle]) {
-            end = middle - 1;
-        } else {
-            start = middle + 1;
-        }
-        middle = Math.floor((start + end) / 2);
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
     }
-    if (arr[middle] === elem) {
-        return middle;
-    }
-    return -1;
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem) {
+    return middle;
+  }
+  return -1;
 }
 binarySearch([1, 2, 5, 6, 7, 9, 11, 34], 11);
 
-
 // Naive Search
 function naiveSearch(long, short) {
-    var count = 0;
-    for (let i = 0; i < long.length; i++) {
-        for (let j = 0; j < short.length; j++) {
-            if (short[j] !== long[i + j]) break;
-            if (j === short.length - 1) count++;
-        }
+  var count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
     }
-    return count;
+  }
+  return count;
 }
 naiveSearch("lorie loled", "lo");
 
